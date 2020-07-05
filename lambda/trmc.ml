c75f94768351ea686914cf5175d9c55f565c736b
Gabriel Scherer <gabriel.scherer@gmail.com>
2020-07-05 07:46:12 +0200

    trmc: warn if there is no TRMC optimization opportunity

--- lambda/trmc.ml
+++ lambda/trmc.ml
@@ -812,6 +812,13 @@ and traverse_binding ctx (var, def) =
   | Some lfun ->
   let cand = Ident.Map.find var ctx.candidates in
   let cand_choice = choice ctx lfun.body in
+  begin match cand_choice with
+  | Choice.Set _ -> ()
+  | Choice.Return _ ->
+      Location.prerr_warning
+        (Debuginfo.Scoped_location.to_location lfun.loc)
+        Warnings.Unused_trmc_attribute;
+  end;
   let direct =
     Lfunction { lfun with body = Choice.direct cand_choice } in
   let dps =
