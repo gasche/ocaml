0306e7a71b94e5e15b559b8c8d4bcbd84e00fbef
Gabriel Scherer <gabriel.scherer@gmail.com>
2024-08-24 22:49:08 +0200

    [testsuite] test the compilation of atomic functions

---
+++ testsuite/tests/atomic-locs/cmm.ml
@@ -0,0 +1,18 @@
+(* TEST_BELOW *)
+
+(* standard atomics *)
+let standard_atomic_get (r : 'a Atomic.t) =
+  Atomic.get r
+
+let standard_atomic_cas (r : 'a Atomic.t) oldv newv =
+  Atomic.compare_and_set r oldv newv
+
+(* TEST
+   not-windows; (* MSVC uses a different symbol separator *)
+   no-flambda; (* the output will be slightly different under Flambda *)
+   no-tsan; (* TSan modifies the generated code *)
+   setup-ocamlopt.byte-build-env;
+   flags = "-c -dcmm -dno-locations -dno-unique-ids";
+   ocamlopt.byte;
+   check-ocamlopt.byte-output;
+*)
