b2d808cd210b6933676f0b6de5b7345f70140bb0
Gabriel Scherer <gabriel.scherer@gmail.com>
2024-08-25 02:14:00 +0200

    add tests for atomic fields

--- testsuite/tests/atomic-locs/cmm.ml
+++ testsuite/tests/atomic-locs/cmm.ml
@@ -7,6 +7,17 @@ let standard_atomic_get (r : 'a Atomic.t) =
 let standard_atomic_cas (r : 'a Atomic.t) oldv newv =
   Atomic.compare_and_set r oldv newv
 
+
+(* atomic record fields *)
+
+type 'a atomic = { filler : unit; mutable x : 'a [@atomic] }
+
+let get (r : 'a atomic) : 'a =
+  r.x
+
+let set (r : 'a atomic) v =
+  r.x <- v
+
 (* TEST
    no-flambda; (* the output will be slightly different under flambda, meh. *)
    setup-ocamlopt.byte-build-env;
