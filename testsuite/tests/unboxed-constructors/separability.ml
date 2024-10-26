77f3c24046f41ab414ad974d33a496da33db5859
Gabriel Scherer <gabriel.scherer@gmail.com>
2024-10-26 17:27:02 +0200

    unboxed constructors: a testsuite for separability

---
+++ testsuite/tests/unboxed-constructors/separability.ml
@@ -0,0 +1,27 @@
+(* TEST
+   expect;
+*)
+
+type t = Float of float [@unboxed] | Foo
+[%%expect {|
+Line 1, characters 0-40:
+1 | type t = Float of float [@unboxed] | Foo
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: This type declaration is non-separated, it contains both float and non-float values.
+|}]
+
+type t = Float of float [@unboxed] | Abstract of abstract [@unboxed]
+[%%expect {|
+Line 1, characters 49-57:
+1 | type t = Float of float [@unboxed] | Abstract of abstract [@unboxed]
+                                                     ^^^^^^^^
+Error: Unbound type constructor "abstract"
+|}]
+
+(* This is unexpected, we need such GADTs to be rejected
+   as they also break separability. *)
+type t = Any : 'a -> t [@unboxed]
+[%%expect {|
+type t = Any : 'a -> t [@unboxed]
+|}]
+
